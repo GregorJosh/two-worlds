f9ac338927dcf6bacf45f060d55dcde425c8ea7b
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-23 23:54:34 +0100

    add 3d boxes animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,15 +1,37 @@
-'use client'
+"use client";
 
-import { JSX, useRef, useEffect, RefObject } from "react";
+import { useRef } from "react";
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
 
-export const Hero = (): JSX.Element => {
-  const divRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+interface BoxMeshProps extends MeshProps {
+  color: string;
+}
 
-  useEffect((): void => {
-    if (typeof window !== "undefined") {
-      console.log(window);
+const Box = (props: BoxMeshProps) => {
+  const meshRef = useRef<Mesh>(null);
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta;
     }
-  }, []);
+  });
 
-  return <div ref={divRef}></div>;
+  return (
+    <mesh {...props} ref={meshRef} scale={2}>
+      <meshStandardMaterial color={props.color} />
+      <boxGeometry args={[1, 1, 1]} />
+    </mesh>
+  );
+};
+
+export const Hero = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.08} />
+      <pointLight position={[0, 3, 0]} intensity={10} />
+      <Box position={[-2, 0, 1]} color="red" />
+      <Box position={[2, 0, 0]} color="blue" />
+    </Canvas>
+  );
 };
