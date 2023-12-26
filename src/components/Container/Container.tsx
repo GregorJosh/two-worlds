5be2903d48e2bfde0ef64f126e6dc80c242d5a27
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-26 22:13:05 +0100

    add styles injection feature

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,16 @@
-import { JSX, ReactNode } from "react";
+import { ReactNode } from "react";
 
 import styles from "./Container.module.scss";
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
-export const Container = (props: Props): JSX.Element => {
-  return <div className={styles.container}>{props.children}</div>;
+export const Container = (props: Props) => {
+  return (
+    <div className={`${styles.container} ${props.className}`}>
+      {props.children}
+    </div>
+  );
 };
