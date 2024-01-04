f2093f22d122511e5215ac68768dd3b0497b7f45
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 19:17:31 +0100

    add new components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from "./Container/Container";
 export * from "./Header/Header";
-export * from "./Hero/Hero";
+export * from "./Menu/Menu";
 export * from "./MenuItem/MenuItem";
+export * from "./Article/Article";
+export * from "./SolarSystem/SolarSystem";
