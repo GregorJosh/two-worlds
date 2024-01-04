149f00e7321cae5dcdeea7e039559c6239f9d51c
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 19:19:09 +0100

    added use of Menu component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,12 @@
-import { Container, MenuItem } from "..";
+import { Container, Menu } from "..";
 
 import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Container>
-        <nav>
-          <ul className={styles.menu}>
-            <MenuItem href="/">My Two Worlds</MenuItem>
-            <MenuItem href="/dev">My Developer World</MenuItem>
-            <MenuItem href="/music">My Music World</MenuItem>
-          </ul>
-        </nav>
+      <Container className={styles.container}>
+        <Menu />
       </Container>
     </header>
   );
