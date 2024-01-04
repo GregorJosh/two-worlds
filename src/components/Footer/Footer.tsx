823d3b42c54e0c025941e0624756d6b9322b02f2
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 22:47:27 +0100

    add toggle LoginModal feature

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,27 @@
-import { Container } from "..";
+"use client";
+
+import { useState } from "react";
+
+import { Container, LoginModal } from "..";
 
 import styles from "./Footer.module.scss";
 
 export const Footer = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
     <footer className={styles.footer}>
       <Container>
-        <p>© 2024 Grzegorz Jóźwiak</p>
+        <p>
+          © 2024{" "}
+          <a href="#" onClick={() => setIsModalVisible(true)}>
+            Grzegorz Jóźwiak
+          </a>
+        </p>
       </Container>
+      {isModalVisible && (
+        <LoginModal onClose={() => setIsModalVisible(!isModalVisible)} />
+      )}
     </footer>
   );
 };
