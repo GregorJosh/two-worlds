3966652fd17d8bdd271d5d55f6d56a8f89f2536c
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 21:15:02 +0100

    add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from "react";
 import { Rubik } from "next/font/google";
 
+import { Container, Footer, Header, SolarSystem } from "@/components";
+
 import "./global.scss";
 import styles from "./layout.module.scss";
-
-import { Container, Header, SolarSystem } from "@/components";
 
 interface Props {
   children: ReactNode;
@@ -21,6 +21,7 @@ export default function RootLayout(props: Props) {
           <SolarSystem />
           {props.children}
         </Container>
+        <Footer />
       </body>
     </html>
   );
