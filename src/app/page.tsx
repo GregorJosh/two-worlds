651c6b10a6a93a73f67b5bcd3c90029219ec6f53
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 15:09:20 +0100

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,15 @@ export const metadata = {
 };
 
 export default function HomePage() {
-  return <h1>My Two Worlds</h1>;
+  return (
+    <>
+      <h1>My Two Worlds</h1>
+      <p>
+        Welcome to my website. This website is the official home of my projects,
+        portfolios and posts dedicated to my two passions: music and
+        programming. My name is Grzegorz Jóźwiak and creating, mixing electronic
+        music and programming are my two favorite activities.
+      </p>
+    </>
+  );
 }
