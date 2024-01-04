0f5252b084095fd26511116a33a5293ae7c6015c
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-04 19:13:57 +0100

    add Rubik font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
 import { ReactNode } from "react";
+import { Rubik } from "next/font/google";
 
 import "./global.scss";
 import styles from "./layout.module.scss";
 
-import { Container, Header, Hero } from "@/components";
+import { Container, Header, SolarSystem } from "@/components";
 
 interface Props {
   children: ReactNode;
 }
 
+const font = Rubik({ subsets: ["latin"] });
+
 export default function RootLayout(props: Props) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body>
         <Header />
-        <Hero />
-        <Container className={styles.container}>{props.children}</Container>
+        <Container className={styles.container}>
+          <SolarSystem />
+          {props.children}
+        </Container>
       </body>
     </html>
   );
