683c5c1707614889eaa0efe4447426a8152f4e2a
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-11 21:50:21 +0100

    create user model

---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+import { models, model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (document, result) => {
+    delete result._id;
+    delete result.hash;
+  },
+});
+
+export default models.User || model("User", userSchema);
