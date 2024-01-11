a50e93b33a6b3130e3abce80c8f6fc4a7e445816
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-12 00:05:03 +0100

    fix db connecting

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 import user from "@/models/user";
 
-mongoose.connect(process.env.DB_HOST!, { tls: true });
+mongoose.connect(process.env.DB_HOST!);
 mongoose.Promise = global.Promise;
 
 export const db = { user };
