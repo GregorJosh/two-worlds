f463f6fe222b799fb6f512146613009d529931b5
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-12 21:17:29 +0100

    test

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,5 @@ import mongoose from "mongoose";
 import user from "@/models/user";
 
 mongoose.connect(process.env.DB_HOST!);
-console.log("db connected successfully");
 
 export const db = { user };
