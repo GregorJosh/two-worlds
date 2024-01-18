d754adc0649ab77e975053b488ae4c79315f5ee7
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-18 21:03:42 +0100

    create article model

---
+++ src/models/article.ts
@@ -0,0 +1,25 @@
+import { models, model, Schema } from "mongoose";
+
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+articleSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+});
+
+export const Article = models.Article || model("Article", articleSchema);
