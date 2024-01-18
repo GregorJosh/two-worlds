c6123af86ea17d7e6a0f7bef5c4ffc7a2c6a68d0
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-18 19:19:48 +0100

    move components to other layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import { ReactNode } from "react";
 
-import { Container, Footer, Header, SolarSystem } from "@/components";
 import { rubik } from "@/fonts";
 
 import "@/styles/global.scss";
-import styles from "./layout.module.scss";
 
 interface Props {
   children: ReactNode;
@@ -13,14 +11,7 @@ interface Props {
 export default function RootLayout(props: Props) {
   return (
     <html lang="en">
-      <body className={`${rubik.variable}`}>
-        <Header />
-        <Container className={styles.container}>
-          <SolarSystem />
-          {props.children}
-        </Container>
-        <Footer />
-      </body>
+      <body className={`${rubik.variable}`}>{props.children}</body>
     </html>
   );
 }
