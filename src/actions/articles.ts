45e14f3534297b74fb67dd3ae4619446a219d952
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-19 19:24:15 +0100

    added connection checking

--- src/actions/articles.ts
+++ src/actions/articles.ts
@@ -3,11 +3,17 @@
 import { db } from "@/libs";
 
 export const getArticleByTitle = async (title: string) => {
-  const article = await db.Article.findOne<Article>({ title }).lean<Article>();
+  if (db.isConnected) {
+    const article = await db.Article.findOne({
+      title,
+    }).lean<Article>();
 
-  if (!article) {
-    throw `Article with ${title} title does not exist!`;
+    if (!article) {
+      throw `Article with ${title} title does not exist!`;
+    }
+
+    return article;
   }
 
-  return article;
+  return null;
 };
