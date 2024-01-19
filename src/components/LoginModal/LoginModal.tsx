460a1f29f5cf656c99504ee9ff69ce3093c48dbb
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-19 23:34:05 +0100

    added form component

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,4 +1,7 @@
-import { Container, LoginForm, Window } from "..";
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+import { Container, Form, FormField, Window } from "..";
 
 import styles from "./LoginModal.module.scss";
 
@@ -6,12 +9,60 @@ interface Props {
   onClose: () => void;
 }
 
+interface FormElements extends HTMLFormControlsCollection {
+  username: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
 export const LoginModal = (props: Props) => {
+  const router = useRouter();
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    const formElement = event.target as HTMLFormElement;
+    const { username, password } = formElement.elements as FormElements;
+
+    const result = await fetch("/api/auth/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+    });
+
+    const data: ResponseBody = await result.json();
+
+    if (data.status === "error") {
+      return data.message;
+    }
+
+    router.back();
+    router.refresh();
+  };
+
   return (
     <div className={styles.backdrop}>
       <Container className={styles.container}>
         <Window title="Log in to Admin Panel" onClose={props.onClose}>
-          <LoginForm />
+          <Form onSubmit={onSubmit} buttonClassName={styles["submit-btn"]}>
+            <FormField label="Username:">
+              <input
+                className={styles.input}
+                type="text"
+                name="username"
+                placeholder="Username"
+                autoComplete="username"
+              />
+            </FormField>
+            <FormField label="Password:">
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                placeholder="Password"
+                autoComplete="new-password"
+              />
+            </FormField>
+          </Form>
         </Window>
       </Container>
     </div>
