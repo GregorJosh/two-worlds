adbd8407f8fa945283c23592b3978b67715720f8
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-19 23:31:28 +0100

    added state management

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,7 @@
-import { FormHTMLAttributes, ReactElement } from "react";
+"use client";
+
+import { FormEvent, FormHTMLAttributes, ReactElement, useState } from "react";
+import { useFormStatus } from "react-dom";
 
 import { FormField } from "..";
 
@@ -8,13 +11,43 @@ type FormField = typeof FormField;
 
 interface Props extends FormHTMLAttributes<HTMLFormElement> {
   children: ReactElement<FormField>[] | ReactElement<FormField>;
+  onSubmit?: OnSubmitHandler;
+  error?: string;
   className?: string;
+  buttonClassName?: string;
 }
 
 export const Form = (props: Props) => {
+  const [error, setError] = useState<string | boolean>(false);
+  const { pending } = useFormStatus();
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (props.onSubmit) {
+      const error = await props.onSubmit(event);
+
+      if (error) {
+        setError(error);
+        setTimeout(() => setError(false), 5000);
+      }
+    }
+  };
+
   return (
-    <form className={`${styles.form} ${props.className}`}>
+    <form
+      className={`${styles.form} ${props.className}`}
+      onSubmit={onSubmit}
+      action={props.action}
+    >
+      {error && <p>{error}</p>}
       {props.children}
+      <button
+        className={`${props.buttonClassName} ${styles.button}`}
+        aria-disabled={pending}
+      >
+        Submit
+      </button>
     </form>
   );
 };
