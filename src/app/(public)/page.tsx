691ed1dd7a09629205a9efb2055fc3558c2778cc
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-19 17:41:17 +0100

    added content management

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,27 +1,50 @@
 import { CSSProperties } from "react";
 
-import { Article } from "@/components";
+import { isAuthenticated } from "@/libs";
+import { Article, Form, FormField } from "@/components";
+import { getArticleByTitle } from "@/actions";
 
 export const metadata = {
   title: "Two Worlds: Home",
 };
 
-export default function HomePage() {
+export default async function HomePage() {
+  const isAuth = isAuthenticated();
+  const article = await getArticleByTitle("Two Worlds");
   const styledP: CSSProperties = {
     textAlign: "left",
   };
 
   return (
-    <Article title="Two Worlds">
-      <p style={styledP}>
-        Welcome to my website. This website is the official home of my projects,
-        portfolios and posts dedicated to my two passions: music and
-        programming.
-      </p>
-      <p style={styledP}>
-        My name is Grzegorz Jóźwiak and creating, mixing electronic music and
-        programming are my two favorite activities.
-      </p>
-    </Article>
+    <>
+      {isAuth && (
+        <Form>
+          <FormField label="Title">
+            <input type="text" name="title" value={article.title} />
+          </FormField>
+          <>
+            {article.content.map((paragraph, id) => {
+              return (
+                <FormField key={id} label="Paragraph">
+                  <textarea
+                    name="paragraph"
+                    defaultValue={paragraph}
+                  ></textarea>
+                </FormField>
+              );
+            })}
+          </>
+        </Form>
+      )}
+      <Article title={article.title}>
+        {article.content.map((paragraph, id) => {
+          return (
+            <p style={styledP} key={id}>
+              {paragraph}
+            </p>
+          );
+        })}
+      </Article>
+    </>
   );
 }
