5533076027f557ed39e09b5026445415e970451b
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-20 22:16:25 +0100

    added query function

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Document, FilterQuery, Model } from "mongoose";
 
 import * as models from "@/models";
 
@@ -6,7 +6,7 @@ let isConnected = false;
 
 if (process.env.DB_HOST) {
   mongoose.connect(process.env.DB_HOST);
-  
+
   isConnected = true;
 }
 
@@ -14,3 +14,16 @@ export const db = {
   isConnected,
   ...models,
 };
+
+export function query<DocumentType>(
+  model: Model<DocumentType>,
+  filter: FilterQuery<DocumentType>
+) {
+  if (db.isConnected) {
+    const result = model.findOne<DocumentType & Document>(filter);
+
+    return result;
+  }
+
+  throw "Not connected to db!";
+}
