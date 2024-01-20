4ea7eacddfc6395974e33d34d0df79cbaf0c273f
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-20 22:10:49 +0100

    added new server actions

--- src/actions/articles.ts
+++ src/actions/articles.ts
@@ -1,19 +1,32 @@
 "use server";
 
-import { db } from "@/libs";
+import { db, query } from "@/libs";
 
-export const getArticleByTitle = async (title: string) => {
-  if (db.isConnected) {
-    const article = await db.Article.findOne({
-      title,
-    }).lean<Article>();
+const queryForArticleByTitle = (title: string) => {
+  const result = query(db.Article, { title });
 
-    if (!article) {
-      throw `Article with ${title} title does not exist!`;
-    }
-
-    return article;
+  if (!result) {
+    throw `Article with ${title} title does not exist!`;
   }
 
-  return null;
+  return result;
+};
+
+export const getArticleByTitle = (title: string) => {
+  const article = queryForArticleByTitle(title);
+
+  return article.lean();
+};
+
+export const updateArticleByTitle = async (
+  title: string,
+  formData: FormData
+) => {
+  const article = await queryForArticleByTitle(title);
+
+  if (article) {
+    article.title = formData.get("title") as string;
+    article.content = formData.getAll("paragraph") as string[];
+    await article.save();
+  }
 };
