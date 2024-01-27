81cf8b9659926ac5a08b3a7846fd0de9dac318bd
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-27 22:31:01 +0100

    added on-success handler

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,36 +1,14 @@
-import { FormEvent } from "react";
 import { useRouter } from "next/navigation";
 
+import { signIn } from "@/actions";
 import { Container, Form, FormField, Window } from "..";
 
 import styles from "./LoginModal.module.scss";
 
-interface FormElements extends HTMLFormControlsCollection {
-  username: HTMLInputElement;
-  password: HTMLInputElement;
-}
-
 export const LoginModal = (props: ModalProps) => {
   const router = useRouter();
 
-  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    const formElement = event.target as HTMLFormElement;
-    const { username, password } = formElement.elements as FormElements;
-
-    const result = await fetch("/api/auth/signin", {
-      method: "POST",
-      body: JSON.stringify({
-        username: username.value,
-        password: password.value,
-      }),
-    });
-
-    const data: ResponseBody = await result.json();
-
-    if (data.status === "error") {
-      return data.message;
-    }
-
+  const onSuccess = () => {
     router.back();
     router.refresh();
   };
@@ -39,9 +17,14 @@ export const LoginModal = (props: ModalProps) => {
     <div className={styles.backdrop}>
       <Container className={styles.container}>
         <Window title="Log in to Admin Panel" onClose={props.onClose}>
-          <Form onSubmit={onSubmit} buttonClassName={styles["submit-btn"]}>
+          <Form
+            buttonClassName={styles["submit-btn"]}
+            action={signIn}
+            onSuccess={onSuccess}
+          >
             <FormField label="Username:">
               <input
+                id="username"
                 className={styles.input}
                 type="text"
                 name="username"
@@ -51,6 +34,7 @@ export const LoginModal = (props: ModalProps) => {
             </FormField>
             <FormField label="Password:">
               <input
+                id="password"
                 className={styles.input}
                 type="password"
                 name="password"
