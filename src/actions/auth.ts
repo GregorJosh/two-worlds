5c8fc3c4a0b28978b6008a67c217e122cda03ae0
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-27 22:27:27 +0100

    added return of status

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,31 +1,35 @@
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
+"use server";
 
-import { authenticate, db } from "@/libs";
+import { authenticate, db, isAuth } from "@/libs";
 import { authSigninSchema } from "@/schemas";
 
-export const signIn = async (formData: FormData) => {
-  const username = formData.get("username");
-  const password = formData.get("password");
+export const signIn = async (formData: FormData): Promise<ActionResult> => {
+  try {
+    const username = formData.get("username");
+    const password = formData.get("password");
 
-  await authSigninSchema.validateAsync({ username, password });
+    await authSigninSchema.validateAsync({ username, password });
 
-  const user = await db.User.findOne({ username }).lean();
+    const user = await db.User.findOne({ username }).lean();
 
-  if (!(user && user.password === password)) {
-    throw "Username or Password is incorrect!";
+    if (!(user && user.password === password)) {
+      throw "Username or Password is incorrect!";
+    }
+
+    await authenticate({ sub: user._id, username });
+
+    return {
+      status: "success",
+      message: "User successfully logged in",
+    };
+  } catch (error: any) {
+    return {
+      status: "error",
+      message: error.message ?? error,
+    };
   }
-
-  await authenticate({ sub: user._id, username });
 };
 
 export const getIsAuth = () => {
-  try {
-    const accessToken = cookies().get("accessToken")?.value ?? "";
-    const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET_KEY!);
-
-    return decodedToken.sub as string;
-  } catch {
-    return false;
-  }
+  return isAuth();
 };
