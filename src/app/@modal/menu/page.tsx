474646ce93e94f06b4b1ec2460d38a7cac83e1c4
GregorJosh <grzegorz.jozw@gmail.com>
2024-01-30 16:27:09 +0100

    added modal slot pages

--- src/app/@modal/menu/page.tsx
+++ src/app/@modal/menu/page.tsx
@@ -1,11 +1,30 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
-import { MenuModal } from "@/components";
+import { Backdrop, Menu, Footer } from "@/components";
 
-export default function MenuModalPage() {
+import styles from "./page.module.scss";
+
+export default function MenuModal() {
   const router = useRouter();
+  const pathname = usePathname();
 
-  return <MenuModal onClose={() => router.back()} />;
+  const onClose = () => {
+    router.back();
+  };
+
+  if (pathname !== "/menu") {
+    return null;
+  }
+
+  return (
+    <Backdrop containerClassName={styles.container}>
+      <Menu navClassName={styles.nav} ulClassName={styles.ul} />
+      <button className={styles.button} onClick={onClose} type="button">
+        &#9587;
+      </button>
+      <Footer />
+    </Backdrop>
+  );
 }
--- src/app/@modal/signin/page.tsx
+++ src/app/@modal/signin/page.tsx
@@ -1,11 +1,58 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
-import { LoginModal } from "@/components";
+import { signIn } from "@/actions";
+import { Backdrop, Window, Form, FormField } from "@/components";
 
-export default function SigninModalPage() {
+import styles from "./page.module.scss";
+
+export default function SigninModal() {
   const router = useRouter();
+  const pathname = usePathname();
 
-  return <LoginModal onClose={() => router.back()} />;
+  const onClose = () => {
+    router.back();
+  };
+
+  const onSuccess = () => {
+    router.push("/");
+  };
+
+  if (pathname !== "/signin") {
+    return null;
+  }
+
+  return (
+    <Backdrop>
+      <Window title="Log in to Admin Panel" onClose={onClose}>
+        <Form
+          buttonClassName={styles["submit-btn"]}
+          action={signIn}
+          onSuccess={onSuccess}
+        >
+          <FormField label="Username:">
+            <input
+              id="username"
+              className={styles.input}
+              type="text"
+              name="username"
+              placeholder="Username"
+              autoComplete="username"
+            />
+          </FormField>
+          <FormField label="Password:">
+            <input
+              id="password"
+              className={styles.input}
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="new-password"
+            />
+          </FormField>
+        </Form>
+      </Window>
+    </Backdrop>
+  );
 }
