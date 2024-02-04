a897cc27299fcd3870c60f75848842ab71a0e421
GregorJosh <grzegorz.jozw@gmail.com>
2024-02-04 18:59:47 +0100

    added use of is-auth-context

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,27 @@
+"use client";
+
+import { useContext } from "react";
 import Link from "next/link";
 
-import { Container } from "..";
+import { Container, IsAuthContext } from "..";
 
 import styles from "./Footer.module.scss";
 
 export const Footer = (props: PropsWithClassName) => {
+  const isAuth = useContext(IsAuthContext);
+
   return (
     <footer className={`${styles.footer} ${props.className}`}>
       <Container className={styles.container}>
         <p>
           © 2024{" "}
-          <Link href="/signin" scroll={false}>
-            Grzegorz Jóźwiak
-          </Link>
+          {!isAuth ? (
+            <Link href="/signin" scroll={false}>
+              Grzegorz Jóźwiak
+            </Link>
+          ) : (
+            "Grzegorz Jóźwiak"
+          )}
         </p>
       </Container>
     </footer>
