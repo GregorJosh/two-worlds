84c3b1eacc099a7d5bbbcdb5a0913f6a085777dd
GregorJosh <grzegorz.jozw@gmail.com>
2024-02-04 18:58:31 +0100

    refactorings and sign-out
    added refactorings, sign-out server action and use of is-auth-context

--- src/app/@modal/menu/page.tsx
+++ src/app/@modal/menu/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 
+import { useContext } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
-import { Backdrop, Menu, Footer } from "@/components";
+import { signOut } from "@/actions";
+import { Backdrop, Menu, Footer, IsAuthContext, MenuItem } from "@/components";
 
 import styles from "./page.module.scss";
 
 export default function MenuModal() {
+  const isAuth = useContext(IsAuthContext);
   const router = useRouter();
   const pathname = usePathname();
 
   const onClose = () => {
     router.back();
+  };
+
+  const onSignOut = () => {
+    signOut();
+    router.back();
+    router.refresh();
   };
 
   if (pathname !== "/menu") {
@@ -20,7 +29,16 @@ export default function MenuModal() {
 
   return (
     <Backdrop containerClassName={styles.container}>
-      <Menu navClassName={styles.nav} ulClassName={styles.ul} />
+      <Menu navClassName={styles.nav} ulClassName={styles.ul}>
+        <MenuItem href="/">Home</MenuItem>
+        <MenuItem href="/dev">Dev World</MenuItem>
+        <MenuItem href="/music">Music World</MenuItem>
+        {isAuth && (
+          <MenuItem href="/" onClick={onSignOut}>
+            Sign out
+          </MenuItem>
+        )}
+      </Menu>
       <button className={styles.button} onClick={onClose} type="button">
         &#9587;
       </button>
