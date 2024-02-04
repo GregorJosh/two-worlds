48790d9c48032c7d1b328e68b5691c00c600a727
GregorJosh <grzegorz.jozw@gmail.com>
2024-02-04 18:53:24 +0100

    added Providers and refactorings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
 
-import { rubik } from "@/fonts";
-
+import { isAuth } from "@/libs";
 import {
   Container,
   Footer,
   Header,
   MenuButton,
   SolarSystem,
+  Providers,
 } from "@/components";
+import { rubik } from "@/fonts";
 
 import "@/styles/global.scss";
+
 import styles from "./layout.module.scss";
 
 interface Props {
@@ -23,7 +25,7 @@ export const dynamic = "force-dynamic";
 export default function RootLayout(props: Props) {
   return (
     <html lang="en">
-      <body className={`${rubik.variable}`}>
+      <body className={rubik.variable}>
         <Header className={styles.header} />
         <Container className={styles.container}>
           <SolarSystem />
@@ -31,7 +33,7 @@ export default function RootLayout(props: Props) {
         </Container>
         <Footer className={styles.footer} />
         <MenuButton />
-        {props.modal}
+        <Providers isAuth={isAuth()}>{props.modal}</Providers>
       </body>
     </html>
   );
