8d68f482e42c2762684e0b3333c4c24dfbfa2513
GregorJosh <grzegorz.jozw@gmail.com>
2024-02-24 23:27:56 +0100

    added displaying images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { revalidatePath } from "next/cache";
+import Image from "next/image";
 
 import { isAuth } from "@/libs";
-import { Article, Form, FormField } from "@/components";
-import { getArticle, updateArticle } from "@/actions";
+import { getArticle, removeImage, updateArticle } from "@/actions";
+import { Article, Button, Form, FormField } from "@/components";
+
+import styles from "./page.module.scss";
 
 export const metadata = {
   title: "Two Worlds: Home",
@@ -13,22 +16,31 @@ export default async function HomePage() {
   const article = await getArticle(articleName);
   const auth = isAuth();
 
-  const updateArticleAction = async (formData: FormData) => {
+  const onUpdateArticle: ActionHandler = async (formData) => {
     "use server";
 
     const result = await updateArticle(formData, articleName);
 
     if (result.status === "success") {
       revalidatePath("/", "layout");
+
+      return;
     }
 
     return result;
   };
 
+  const onRemoveImage = async (formData: FormData) => {
+    "use server";
+
+    await removeImage(formData, articleName);
+    revalidatePath("/", "layout");
+  };
+
   return (
     <>
       {auth && (
-        <Form action={updateArticleAction}>
+        <Form action={onUpdateArticle}>
           <FormField label="Title">
             <input type="text" name="title" defaultValue={article.title} />
           </FormField>
@@ -44,11 +56,35 @@ export default async function HomePage() {
               );
             })}
           </>
+          <FormField label="Image">
+            <input type="file" name="image" accept="image/*" />
+          </FormField>
         </Form>
       )}
       <Article title={article.title}>
         {article.content.map((paragraph, id) => {
           return <p key={id}>{paragraph}</p>;
+        })}
+        {article.images.map((image, id) => {
+          return (
+            <div key={id} className={styles.image}>
+              <Image
+                src={`/${image}`}
+                alt={image}
+                sizes="(max-width: 768px) 100vw"
+                fill
+                priority
+              />
+              {auth && (
+                <form action={onRemoveImage}>
+                  <input type="hidden" name="image-filename" value={image} />
+                  <Button className={styles.button} type="submit">
+                    REMOVE
+                  </Button>
+                </form>
+              )}
+            </div>
+          );
         })}
       </Article>
     </>
