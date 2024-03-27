d1efaf2b828b7a95c4335f7cc0eff4baa3243260
GregorJosh <grzegorz.jozw@gmail.com>
2024-03-27 19:43:15 +0100

    added upload adapter

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,14 +1,62 @@
-import type { EditorConfig } from "@ckeditor/ckeditor5-core";
+import type { EditorConfig, PluginConstructor } from "@ckeditor/ckeditor5-core";
+import type { EventInfo } from "@ckeditor/ckeditor5-utils";
+import type { FileLoader, UploadResponse } from "@ckeditor/ckeditor5-upload";
 
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import Editor from "ckeditor5/ckeditor";
 
-const editorConfig: EditorConfig = {
-  placeholder: "Article content...",
-};
+interface Props {
+  onReady?: (editor: Editor) => void;
+  onImageUpload: (file: File) => Promise<UploadResponse>;
+  content?: string;
+}
 
-function RichTextEditor({ content }: { content?: string }) {
-  return <CKEditor editor={Editor} data={content} config={editorConfig} />;
+function RichTextEditor({ onReady, onImageUpload, content }: Props) {
+  class UploadAdapter {
+    loader: FileLoader;
+
+    constructor(loader: FileLoader) {
+      this.loader = loader;
+    }
+
+    async upload(): Promise<UploadResponse> {
+      try {
+        const file = (await this.loader.file) as File;
+
+        return await onImageUpload(file);
+      } catch {
+        return {};
+      }
+    }
+
+    abort() {}
+  }
+
+  function UploadAdapterPlugin(editor: Editor) {
+    const FileRepositoryPlugin = editor.plugins.get("FileRepository");
+
+    FileRepositoryPlugin.createUploadAdapter = (loader) => {
+      return new UploadAdapter(loader);
+    };
+  }
+
+  const editorConfig: EditorConfig = {
+    extraPlugins: [UploadAdapterPlugin as PluginConstructor],
+  };
+
+  const onFocus = (event: EventInfo, editor: Editor) => {
+    event.stop();
+  };
+
+  return (
+    <CKEditor
+      editor={Editor}
+      data={content}
+      config={editorConfig}
+      onReady={onReady}
+      onFocus={onFocus}
+    />
+  );
 }
 
 export default RichTextEditor;
