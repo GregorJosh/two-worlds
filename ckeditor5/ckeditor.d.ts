9c3a38e138782982c454638f7d2ca0902b683a81
GregorJosh <grzegorz.jozw@gmail.com>
2024-03-27 19:45:12 +0100

    added ckeditor5

--- ckeditor5/ckeditor.d.ts
+++ ckeditor5/ckeditor.d.ts
@@ -7,11 +7,13 @@ import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { Bold, Italic, Underline } from '@ckeditor/ckeditor5-basic-styles';
 import type { EditorConfig } from '@ckeditor/ckeditor5-core';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Image, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Link } from '@ckeditor/ckeditor5-link';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { Undo } from '@ckeditor/ckeditor5-undo';
 declare class Editor extends ClassicEditor {
-    static builtinPlugins: (typeof Alignment | typeof Bold | typeof Essentials | typeof Image | typeof ImageUpload | typeof Italic | typeof Paragraph | typeof Underline | typeof Undo)[];
+    static builtinPlugins: (typeof Alignment | typeof Bold | typeof Essentials | typeof Heading | typeof Image | typeof ImageUpload | typeof Italic | typeof Link | typeof Paragraph | typeof Underline | typeof Undo)[];
     static defaultConfig: EditorConfig;
 }
 export default Editor;
