ff92940b5ef6565cfba041b294f48a2b94549058
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:38:38 +0200

    add controlled input

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -1,13 +1,38 @@
-import { InputHTMLAttributes, ReactElement } from "react";
+"use client";
+
+import React, {
+  ChangeEventHandler,
+  InputHTMLAttributes,
+  ReactComponentElement,
+  ReactElement,
+  useState,
+} from "react";
 
 import styles from "./FormField.module.scss";
 
-interface Props {
+export const Input = (
+  props: InputHTMLAttributes<HTMLInputElement>
+): React.JSX.Element => {
+  const { value, onChange, ...restProps } = props;
+  const [newValue, setNewValue] = useState<typeof value>(value);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setNewValue(event.target.value);
+  };
+
+  return (
+    <input {...restProps} value={newValue} onChange={handleChange}></input>
+  );
+};
+
+interface FormFieldProps {
   label: string;
-  children: ReactElement<FormInputElement>;
+  children:
+    | ReactElement<FormInputElement>
+    | ReactComponentElement<typeof Input>;
 }
 
-export const FormField = (props: Props) => {
+export const FormField = (props: FormFieldProps): React.JSX.Element => {
   const { children } = props;
 
   return (
