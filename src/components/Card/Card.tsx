20a33e71ede8c1f3f022ea35d48cf35c7c9b4bf6
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:34:38 +0200

    add prop for content styling

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,13 +4,16 @@ import styles from "./Card.module.scss";
 
 interface Props extends PropsWithChildren {
   title: string;
+  contentClassName?: string;
 }
 
 export const Card = async (props: Props) => {
   return (
     <div className={styles.card}>
       <h2>{props.title}</h2>
-      {props.children}
+      <div className={`${styles.content} ${props.contentClassName}`}>
+        {props.children}
+      </div>
     </div>
   );
 };
