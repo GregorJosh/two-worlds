7ceddfc5b97060dda8cf3f12ee66c9a7838e5be8
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:16:44 +0200

    change segment config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ export const metadata = {
   title: "Two Worlds: Home",
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function HomePage() {
+  console.log("Rendering home page...");
+
   return <Article name="home_welcome" />;
 }
