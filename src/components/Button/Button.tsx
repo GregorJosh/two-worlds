649912e5cc7a006bdc3bc02ff31ba567dfe9961b
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:32:44 +0200

    add conditional link rendering

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,40 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import Link from "next/link";
 
 import styles from "./Button.module.scss";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: string;
+  children: ReactNode;
+  redirectTo?: string;
   type?: "button" | "submit";
 }
 
-export const Button = (props: Props) => {
-  const { children, className, type = "button", ...restProps } = props;
+export const Button = (props: Props): React.JSX.Element => {
+  const {
+    children,
+    className,
+    type = "button",
+    redirectTo,
+    ...restProps
+  } = props;
+
+  const newClassName = `${styles.button} ${className}`;
+
+  if (redirectTo) {
+    return (
+      <Link
+        href={redirectTo}
+        scroll={false}
+        prefetch={false}
+        className={newClassName}
+      >
+        {children}
+      </Link>
+    );
+  }
 
   return (
-    <button
-      {...restProps}
-      className={`${styles.button} ${className}`}
-      type={type}
-    >
+    <button {...restProps} className={newClassName} type={type}>
       {children}
     </button>
   );
