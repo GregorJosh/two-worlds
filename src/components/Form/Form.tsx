8fc77cf878f2cfa0f437cfaa721fe3ea9974b398
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:36:45 +0200

    move use-form-state to hooks

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,35 +1,14 @@
 "use client";
 
-import { FormHTMLAttributes, ReactElement, useState } from "react";
+import { FormHTMLAttributes, PropsWithChildren, ReactElement } from "react";
 import { useFormStatus } from "react-dom";
 
-import { FormField, Button } from "..";
+import { FormField, Button, Loader, MessageBox } from "..";
+import { useFormState } from "@/hooks";
 
 import styles from "./Form.module.scss";
 
-const useFormState = (action: ActionHandler) => {
-  const [status, setStatus] = useState<ActionResultStatus>("unknown");
-  const [message, setMessage] = useState<string | false>(false);
-
-  const clearState = () => {
-    setStatus("unknown");
-    setMessage(false);
-  };
-
-  const formAction = async (formData: FormData) => {
-    const result = await action(formData);
-
-    if (result) {
-      setStatus(result.status);
-      setMessage(result.message);
-      setTimeout(clearState, 5000);
-    }
-  };
-
-  return [status, message, formAction] as const;
-};
-
-const Submit = (props: PropsWithClassName) => {
+const Submit = (props: PropsWithChildren<PropsWithClassName>) => {
   const { pending } = useFormStatus();
 
   return (
@@ -39,33 +18,41 @@ const Submit = (props: PropsWithClassName) => {
       disabled={pending}
       aria-disabled={pending}
     >
-      Submit
+      {props.children || "Submit"}
     </Button>
   );
 };
 
-type FormField = typeof FormField;
+type FormField = typeof FormField | HTMLInputElement;
 
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
-  children: ReactElement<FormField>[] | ReactElement<FormField>;
   action: ActionHandler;
+  children?: ReactElement<FormField>[] | ReactElement<FormField>;
   className?: string;
   buttonClassName?: string;
+  buttonLabel?: string | ReactElement;
 }
 
 export const Form = (props: FormProps) => {
-  const { action, children, buttonClassName, ...restProps } = props;
-  const [status, message, formAction] = useFormState(action);
+  const { action, children, buttonClassName, buttonLabel, ...restProps } =
+    props;
+  const [status, message, isLoading, formAction] = useFormState(action);
 
   return (
-    <form
-      {...restProps}
-      className={`${styles.form} ${props.className}`}
-      action={formAction}
-    >
-      {status === "error" && <p>{message}</p>}
-      {children}
-      <Submit className={buttonClassName} />
-    </form>
+    <>
+      {isLoading ? (
+        <Loader inner />
+      ) : (
+        <form
+          {...restProps}
+          className={`${styles.form} ${props.className}`}
+          action={formAction}
+        >
+          {status === "error" && <MessageBox message={message} />}
+          {children}
+          <Submit className={buttonClassName}>{buttonLabel}</Submit>
+        </form>
+      )}
+    </>
   );
 };
