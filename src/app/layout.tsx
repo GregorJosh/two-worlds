e9393382b888ad163a422696cd078cc782f50fdb
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:12:03 +0200

    add context providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,22 @@
 import { ReactNode } from "react";
 
-import { isAuth } from "@/libs";
+import { AuthProvider, PlayerProvider } from "@/context";
+
+import { getIsAuth } from "@/actions";
+
 import {
   Container,
   Footer,
   Header,
   MenuButton,
   SolarSystem,
-  Providers,
 } from "@/components";
+
 import { rubik } from "@/fonts";
 
 import "@/styles/global.scss";
 
 import styles from "./layout.module.scss";
-
-interface Props {
-  children: ReactNode;
-  modal: ReactNode;
-}
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -27,18 +25,33 @@ export const metadata = {
   title: "Two Worlds",
 };
 
-export default function RootLayout(props: Props) {
+export default function RootLayout({
+  children,
+  modal,
+  player,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+  player: ReactNode;
+}) {
+  console.log("Rendering root layout...");
+
   return (
     <html lang="en">
       <body className={rubik.variable}>
-        <Header className={styles.header} />
-        <Container className={styles.container}>
-          <SolarSystem />
-          {props.children}
-        </Container>
-        <Footer className={styles.footer} />
-        <MenuButton />
-        <Providers isAuth={isAuth()}>{props.modal}</Providers>
+        <AuthProvider isAuth={getIsAuth()}>
+          <PlayerProvider>
+            <Header className={styles.header} />
+            <Container className={styles.container}>
+              <SolarSystem />
+              {children}
+            </Container>
+            <Footer className={styles.footer} />
+            <MenuButton />
+            {modal}
+            {player}
+          </PlayerProvider>
+        </AuthProvider>
       </body>
     </html>
   );
