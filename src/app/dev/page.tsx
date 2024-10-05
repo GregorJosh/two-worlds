a1849f5f560633120540803681d9f6af79201c73
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:27:44 +0200

    add portfolio card content

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,14 +1,34 @@
-import { Article, Card } from "@/components";
+import { Article, Card, Repository, Product } from "@/components";
 
 export const metadata = {
   title: "Two Worlds: Dev",
 };
 
-export default function DevPage() {
+export default async function DevPage() {
+  const repos: GitHubRepo[] = await (
+    await fetch("https://api.github.com/users/gregorjosh/repos", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + process.env.GH_TOKEN,
+        Accept: "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    })
+  ).json();
+
+  console.log("Rendering dev page...");
+
   return (
     <>
       <Article name="dev_welcome" />
-      <Card title="Portfolio" />
+      <Card title="Portfolio">
+        {repos.map((repo) => (
+          <Repository key={repo.id} repo={repo} />
+        ))}
+      </Card>
+      <Card title="Products">
+        <Product product={{ name: "HSMS", url: "#" }} />
+      </Card>
     </>
   );
 }
