d16f689f9612d176688812e9de9b1b85958fd749
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:42:39 +0200

    create message-box component

---
+++ src/components/MessageBox/MessageBox.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const MessageBox = ({
+  message,
+  duration = 3000,
+}: {
+  message: string;
+  duration?: number;
+}): React.JSX.Element => {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (message) {
+      setIsVisible(true);
+
+      const timer = setTimeout(() => setIsVisible(false), duration);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [message]);
+
+  return <>{isVisible ? <p>{message}</p> : null}</>;
+};
