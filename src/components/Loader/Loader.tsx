e17822aef7990e59c2def8b04bcc7b0c76589271
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:40:10 +0200

    add inner prop

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { Circles } from "react-loader-spinner";
+
 import styles from "./Loader.module.scss";
 
 interface Props {
+  inner?: boolean;
   message?: string;
 }
 
 export const Loader = (props: Props) => {
-  const { message = "Loading..." } = props;
+  const { inner = false, message = "Loading..." } = props;
+  const className: string = inner
+    ? styles["inner-container"]
+    : styles["outer-container"];
 
   return (
-    <div className={styles.container}>
+    <div className={className}>
+      <Circles wrapperClass={styles.loader} />
       <p>{message}</p>
     </div>
   );
