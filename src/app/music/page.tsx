0fa209aeebac4fd6e85de552f6cb01778e92e08c
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:28:35 +0200

    add tracks card

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,9 +1,31 @@
-import { Article } from "@/components";
+import { Metadata } from "next";
 
-export const metadata = {
+import { getIsAuth, getTracks } from "@/actions";
+import { Article, Card, Button, TrackCard } from "@/components";
+
+export const metadata: Metadata = {
   title: "Two Worlds: Music",
 };
 
-export default function MusicPage() {
-  return <Article content="My Music World" />;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default async function MusicPage(): Promise<React.JSX.Element> {
+  const isAuth: boolean = getIsAuth();
+  const tracks: Documents.Track[] = await getTracks();
+
+  console.log("Rendering music page...");
+
+  return (
+    <>
+      <Article name="music_welcome" />
+      <Card title="Tracks">
+        {tracks.map((track) => (
+          <TrackCard key={track.title} track={track} />
+        ))}
+      </Card>
+      <Card title="DJ Mixes" />
+      {isAuth && <Button redirectTo="/new-track">Add new track</Button>}
+    </>
+  );
 }
