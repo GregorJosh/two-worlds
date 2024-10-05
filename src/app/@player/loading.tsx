02a0044b9659da1730ccc4069bdcc30c71521461
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-05 17:23:39 +0200

    create player loading page

---
+++ src/app/@player/loading.tsx
@@ -0,0 +1,7 @@
+import { Loader } from "@/components";
+
+export default function PlayerLoadingPage() {
+  console.log("Rendering player loading page...");
+  
+  return <Loader message="Loading Player..." />;
+}
