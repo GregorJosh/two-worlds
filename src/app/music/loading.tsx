090957baaedffb82fcb21ef1be58e7936b20fb06
GregorJosh <grzegorz.jozw@gmail.com>
2024-10-06 20:37:31 +0200

    change to async function

--- src/app/music/loading.tsx
+++ src/app/music/loading.tsx
@@ -1,6 +1,6 @@
 import { Loader } from "@/components";
 
-export default function MusicLoadingPage() {
+export default async function MusicLoadingPage() {
   console.log("Rendering music loading page...");
 
   return <Loader message="Loading Music Page..." />;
